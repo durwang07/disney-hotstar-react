b95d31c59fb45e378082fbc9bf3f3a54cd55fe8a
durwangpatole8@gmail.com <durwangpatole8@gmail.com>
2025-01-07 15:27:58 +0530

    api call

--- disney-hotstar/src/Components/Header.jsx
+++ disney-hotstar/src/Components/Header.jsx
@@ -43,14 +43,14 @@ function Header() {
             <img src=  {logo}  className='w-[80px] md:w[115px] object-cover'/>
             <div className=' hidden md:flex gap-8'>
     {menu.map((item)=>(
-        <HeaderItem name={item.name} Icon = {item.icon}/>
+        <HeaderItem key={item.name} name={item.name} Icon = {item.icon}/>
        
    
     ))}
     </div>
     <div className='flex md:hidden gap-5'>
     {menu.map((item,index)=>index<3 &&(
-        <HeaderItem name={''} Icon = {item.icon}/>
+        <HeaderItem key={index} name={''} Icon = {item.icon}/>
        
    
     ))}
