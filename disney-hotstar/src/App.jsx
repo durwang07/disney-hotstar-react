761b7716586d00e97eb8004fa2a8019b0144a02b
durwangpatole8@gmail.com <durwangpatole8@gmail.com>
2025-01-21 17:24:13 +0530

    done

--- disney-hotstar/src/App.jsx
+++ disney-hotstar/src/App.jsx
@@ -5,15 +5,19 @@ import './App.css'
 import Header from './Components/Header'
 import Slider from './Components/Slider'
 import ProductionHouse from './Components/ProductionHouse'
+import GenreMovieList from './Components/GenreMovieList'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div>
       <Header/>
       <Slider/>
       <ProductionHouse/>
+      <GenreMovieList/>
+     
     </div>
   )
 }
--- disney-hotstar/src/Services/GlobalAPI.jsx
+++ disney-hotstar/src/Services/GlobalAPI.jsx
@@ -4,8 +4,15 @@ import axios from "axios";
 const movieBaseUrl = 'https://api.themoviedb.org/3'
 const api_key = '83aa1dbd84e44c99a64fa279ae8c6f2a'
 
-const getTrendingVideos = axios.get(movieBaseUrl +`/trending/all/day?api_key=${api_key}`);
+const movieBygenreBaseURL ='https://api.themoviedb.org/3/discover/movie?api_key=83aa1dbd84e44c99a64fa279ae8c6f2a';
 
+
+
+const getTrendingVideos = axios.get(movieBaseUrl + 
+    "/trending/all/day?api_key=" + api_key);
+    const getMovieByGenreId=(id)=>
+        axios.get(movieBygenreBaseURL + "&width_genres=" +id)
 export default{
-    getTrendingVideos
+    getTrendingVideos,
+    getMovieByGenreId
 }
